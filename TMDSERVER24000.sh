5286264aff98146f0c12ad0257e9c2029eaab4c9
ablk <you@example.com>
2020-01-14 15:36:51 +0000

    sfoghsog

--- TMDSERVER24000.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-gogui-server -verbose -loop -port 24000 "./TCG_NOGO" 2>&1 | tee -a nogo.log
---
+++ connect.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+gogui-twogtp -black "gogui-client 192.168.103.22 24000" -white "/tcg/files/pj-4-test" -size 9 -auto -games 10 -sgffile test -verbose
